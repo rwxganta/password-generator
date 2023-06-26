08030e409bed4b92d47d4b3a3e6e01cf51c118e4
Luka Mendonça <lukamendoncadev@proton.me>
2023-06-26 20:52:40 -0300

    start js code T-T

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const password = document.getElementById('password');
+const lengthCount = document.querySelector('.length-count');
+const range = document.getElementById('length');
+
+const generateBtn = document.getElementById('generate');
+
+
 const LOWERCASE_LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 const UPPERCASE_LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 const NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
@@ -6,4 +13,9 @@ const SYMBOLS = ['!', '@', '#', '$', '%', '&', '*', '(', ')', '-', '+', '=', '['
 
 const MIN_LENGTH = 8;
 const MAX_LENGTH = 32;
+let length = MIN_LENGTH;
 
+range.addEventListener('input', () => {
+    lengthCount.textContent = range.value;
+    length = range.value;
+});
